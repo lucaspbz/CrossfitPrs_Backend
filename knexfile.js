32fd69fcc4ff9d1bf45c16d46ab81b6c78bbb873
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2020-05-04 15:16:17 -0300

    Configuring Heroku Database Connection

--- knexfile.js
+++ knexfile.js
@@ -30,17 +30,14 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
+      directory: __dirname + '/src/database/migrations',
       tableName: 'knex_migrations'
     }
   }
